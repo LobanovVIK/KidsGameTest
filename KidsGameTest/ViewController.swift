8fa9927a23f6d9d5c51018a8aab65b50cd7035ee
LobanovVIK <tel0504003118@gmail.com>
2026-04-21 21:03:06 +0300

    Initial Commit

---
+++ KidsGameTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KidsGameTest
+//
+//  Created by Lobanov Viktor on 21.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
